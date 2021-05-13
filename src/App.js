35dcf22489d3e338929b9fa746e92901dbd97ae2
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-05-13 11:28:05 +0530

    Removed All Unnecessary Things And Made My First Very Simple React App 🎉

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
 import "./App.css";
-import NewContact from "./Contact";
-import AddContact from "./Add";
-import { useState } from "react";
 
 function App() {
-
-  const[conts, setConts] = useState([
-    { name : "Shivansh Pratap", mobile : "9026631952", email : "shivanshsuryavanshi@gmail.com"},
-    { name : "Suryansh Trivedi", mobile : "9876543221", email : "suryanshtrivedi1@gmail.com"}
-  ]);
-
   return (
     <div className="App">
-      
-        <div className = "Contacts">
-          <AddContact />
-          {conts.map(cont => (
-            <NewContact name = {cont.name} mobile = {cont.mobile} email = {cont.email} />
-          ))}
-        </div>
+      <h1>First React Project</h1>
     </div>
   );
 }
